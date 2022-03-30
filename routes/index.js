fc39e30fe8c763e6ac686ceb4788e1666182d42a
Semu112 <83565458+Semu112@users.noreply.github.com>
2022-03-30 23:33:04 +0000

    #4

--- routes/index.js
+++ routes/index.js
@@ -12,5 +12,12 @@ router.get('/last.txt', function(req, res, next) {
   lastGet = new Date();
 });
 
+let colorArray = ["red", "yellow", "green", "blue"];
+let getAmount = 0;
+router.get('/color.html', function(req, res, next) {
+  let toSend = `<h1 style='color: ${colorArray[getAmount%4]}'> ${colorArray[getAmount%4]} </h1>`;
+  res.send(toSend);
+  getAmount++;
+});
 
 module.exports = router;
